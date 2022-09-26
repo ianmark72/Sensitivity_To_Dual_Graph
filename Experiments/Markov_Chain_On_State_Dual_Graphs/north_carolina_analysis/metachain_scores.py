4327ce11699e0985cd093b796881377ee0d018d9
Lorenzo Najt <lnajt4@gmail.com>
2022-09-25 21:27:02 -0400

    Update metachain_scores.py

--- Experiments/Markov_Chain_On_State_Dual_Graphs/north_carolina_analysis/metachain_scores.py
+++ Experiments/Markov_Chain_On_State_Dual_Graphs/north_carolina_analysis/metachain_scores.py
@@ -86,7 +86,7 @@ def gerrychain_score(proposal_graph, graph, config, updaters, epsilon, ideal_pop
 
 
 def test_score(proposal_graph, graph, config, updaters, epsilon, ideal_population, gerrychain_steps, accept, k):
-    print("energy", len(graph.edges()))
+    print("num edges of proposal graph", len(proposal_graph.edges()))
     return np.log(len(proposal_graph.edges()))
 
 def ising_score(proposal_graph, graph, config, updaters, epsilon, ideal_population, gerrychain_steps, accept, k):
