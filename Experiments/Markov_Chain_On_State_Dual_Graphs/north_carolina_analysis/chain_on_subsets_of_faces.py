34699814c5b129121daaeca3fb357c06597a9613
Lorenzo Najt <lnajt4@gmail.com>
2022-09-25 22:36:18 -0400

    switched to gerrychain for search down

--- Experiments/Markov_Chain_On_State_Dual_Graphs/north_carolina_analysis/chain_on_subsets_of_faces.py
+++ Experiments/Markov_Chain_On_State_Dual_Graphs/north_carolina_analysis/chain_on_subsets_of_faces.py
@@ -546,7 +546,7 @@ if __name__ ==  '__main__':
         'WEIGHT_FLIPS' : 0,
         'EXPERIMENT_START': str(datetime.now()),
         'BASE_SCORE': 7.75,
-        'metachain_score' : "test_score" #"test_score" #
+        'metachain_score' : "gerrychain_score" #"test_score" #
     }
     # Seanna: so in here the number of districts is 12 (maybe we want to revise it?)
     main()
--- Experiments/Markov_Chain_On_State_Dual_Graphs/north_carolina_analysis/metachain_scores.py
+++ Experiments/Markov_Chain_On_State_Dual_Graphs/north_carolina_analysis/metachain_scores.py
@@ -82,6 +82,8 @@ def gerrychain_score(proposal_graph, graph, config, updaters, epsilon, ideal_pop
         seats_won_for_democrats.append(dem_seats_won)
 
     seat_score  = statistics.mean(seats_won_for_republicans)
+    seat_score  = statistics.mean(seats_won_for_democrats)
+
     return seat_score
 
 
