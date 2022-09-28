487c1290b4d53a7584c1601eaaabe03bbe8ba25b
Lorenzo Najt <lnajt4@gmail.com>
2022-09-27 22:37:38 -0400

    Update chain_on_subsets_of_faces.py

--- Experiments/Markov_Chain_On_State_Dual_Graphs/north_carolina_analysis/chain_on_subsets_of_faces.py
+++ Experiments/Markov_Chain_On_State_Dual_Graphs/north_carolina_analysis/chain_on_subsets_of_faces.py
@@ -461,12 +461,8 @@ def main():
             #temperature function
             #\ 50\left(\exp\left(\operatorname{mod}\left(x,1500\right)\ \cdot\ -\frac{5}{1500}\right)-\ \exp\left(-5\right)\right)
             if (i == 50) and (score < (threshold_to_beat - .2)):
-                # If sufficiently close top the threshold , keep going
                 logging.info(f"Main    : resetting temp counter, score {score} failed to pass threshold: {(threshold_to_beat - .1)}. starting validation thread %s")
-
-'
-
-'            # print("resetting temp counter, score ", score, "failed to pass threshold", (threshold_to_beat - .1))
+            # print("resetting temp counter, score ", score, "failed to pass threshold", (threshold_to_beat - .1))
                 tmp_ctr = 0
             temperature =  5 * ((math.exp( (tmp_ctr % 1500) * -(5/1500))) - math.exp(-5))
             #temperature = .00001 #for testing
